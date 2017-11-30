2eb79c8da7e17fcfd642ba2809ff2eb97a01661e
gsylvestre <gsylvestre@gmail.com>
2017-11-30 15:50:03 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Guillaume Sylvestre | Formateur en développement web | Nantes</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Formateur freelance en développement web : PHP, Symfony, SQL, Javascript, jQuery, HTML, CSS">
+
+	<link href="//www.google-analytics.com" rel="dns-prefetch">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:700%7CVarela+Round"> 
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/print.css" media="print">
+
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" href="img/favicon.ico" />
+	<meta name="application-name" content="&nbsp;"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="img/mstile-144x144.png" />
+</head>
+<body>
+	<div class="container">
+		<header>
+			<h1>Guillaume Sylvestre</h1>
+			<h2>Formateur&nbsp;en développement&nbsp;web</h2>
+			<div id="intro">
+				<ul>
+				    <li><em>5000+ heures</em> de formations depuis 2013</li>
+				    <li><em>11 ans d'expérience</em> en développement web</li>
+				    <li>Études en <em>psychologie de l'éducation</em></li>
+				</ul>
+			</div>
+		</header>
+		<main>
+			<section id="competences">
+				<h2><span class="fa fa-code-fork"></span> Enseignements</h2>
+				<dl class="skills-list">
+				    <dt>PHP</dt><dd class="skill-100"><div></div></dd>
+				    <dt>Symfony</dt><dd class="skill-95"><div></div></dd>
+				    <dt>SQL / MySQL</dt><dd class="skill-90"><div></div></dd>
+				    <dt>Neo4J / Cypher</dt><dd class="skill-60"><div></div></dd>
+				    <dt>Node.js</dt><dd class="skill-70"><div></div></dd>
+				    <dt>Apache</dt><dd class="skill-70"><div></div></dd>
+				    <dt class="spacer"></dt>
+				    <dt>Javascript</dt><dd class="skill-95"><div></div></dd>
+				    <dt>jQuery</dt><dd class="skill-95"><div></div></dd>
+				    <dt>HTML5 / CSS3</dt><dd class="skill-90"><div></div></dd>
+				    <dt>SASS</dt><dd class="skill-80"><div></div></dd>
+				    <dt>Gulp</dt><dd class="skill-70"><div></div></dd>
+				    <dt>Bootstrap</dt><dd class="skill-80"><div></div></dd>
+				    <dt>PhoneGap</dt><dd class="skill-60"><div></div></dd>
+				    <dt class="spacer"></dt>
+				    <dt>WordPress</dt><dd class="skill-90"><div></div></dd>
+				    <dt>SEO</dt><dd class="skill-80"><div></div></dd>
+				    <dt>Git</dt><dd class="skill-90"><div></div></dd>
+				    <dt>SVN</dt><dd class="skill-60"><div></div></dd>
+				    <dt>APIs Google</dt><dd class="skill-70"><div></div></dd>
+				    <dt>JSON / Yaml / XML</dt><dd class="skill-80"><div></div></dd>
+				</dl>
+				<div id="languages">
+					<h2><span class="fa fa-flag-o"></span> Langues d'enseignement</h2>
+					<ul>
+						<li>Français</li>
+						<li>Anglais</li>
+					</ul>
+				</div>
+			</section>
+			<section id="exp">
+				<h2><span class="fa fa-comments-o"></span> Références</h2>
+				<ul class="refs">
+					<li><span><a href="http://imie-ecole-informatique.fr/" title="IMIE">IMIE</a></span><span>Nantes</span><span>2016&nbsp;-&nbsp;</span></li>
+					<li><span><a href="http://www.eni-ecole.fr/" title="ENI École Informatique">ENI École Informatique</a></span><span>Nantes</span><span>2016&nbsp;-&nbsp;</span></li>
+					<li><span><a href="http://www.wis-ecoles.com/" title="WIS">WIS</a></span><span>Nantes</span><span>2016&nbsp;-&nbsp;</span></li>
+					<li><span><a href="https://www.mistra.fr/" title="Mistra">Mistra</a></span><span>Divers</span><span>2016&nbsp;-&nbsp;</span></li>
+					<li><span><a href="http://www.wf3.fr/" title="WebForce3">WebForce3</a></span><span>Paris</span><span>2014&nbsp;-&nbsp;</span></li>
+					<li><span><a href="https://www.ecole-multimedia.com/" title="L'École Multimédia">L'École Multimédia</a></span><span>Paris</span><span>2014&nbsp;-&nbsp;2016</span></li>
+					<li><span><a href="http://www.nextformation.com/" title="Next Formation">Next Formation</a></span><span>Paris</span><span>2013&nbsp;-&nbsp;2016</span></li>
+					<li><span><a href="http://www.hitema.fr/" title="Hitema">Hitema</a></span><span>Paris</span><span>2013&nbsp;-&nbsp;2016</span></li>
+					<li><span><a href="https://3wa.fr/" title="3W Academy">3W Academy</a></span><span>Paris</span><span>2013&nbsp;-&nbsp;2014</span></li>
+				</ul>
+			</section>
+			<section id="formations">
+				<h2><span class="fa fa-mortar-board"></span> Formations</h2>
+				<ul>
+					<li>Doctorat en psychologie de l'éducation (non complété)<div class="training-info">2004-2005. UQAM, Montréal, Canada</div></li>
+					<li>Licence en psychologie de l'éducation<div class="training-info">2001-2004. UQAM, Montréal, Canada</div></li>
+				</ul>
+			</section>
+			<section id="contact" itemscope itemtype="http://schema.org/Person">
+				<h2><span class="fa fa-phone"></span> Contact</h2>
+				<img src="img/guillaume-sylvestre.jpg" id="me" alt="Guillaume Sylvestre | Formateur en développement web" itemprop="photo">
+				<ul class="contacts">
+					<li itemprop="address" itemscope itemtype="http://schema.org/Place">Région de <span itemprop="addressLocality">Nantes</span>, <span itemprop="addressCountry">France</span></li>
+					<li><span itemprop="telephone">06 34 44 34 92</span></li>
+					<li><a href="mailto:gsylvestre@gmail.com" itemprop="email">gsylvestre@gmail.com</a></li>
+					<li><a itemprop="url" class="website" href="http://guillaumesylvestre.com" title="Site de Guillaume Sylvestre">http://guillaumesylvestre.com</a></li>
+					<li><a itemprop="url" class="cv" href="guillaume-sylvestre.pdf" title="Télécharger mon CV">Curriculum vitae</a></li>
+					<li>
+						<ul class="social">
+							<li><a itemprop="url" href="https://www.linkedin.com/in/gsylvestre" title="Profil LinkedIn | Guillaume Sylvestre" class="fa fa-linkedin"></a></li>
+							<li><a itemprop="url" href="http://fr.viadeo.com/fr/profile/guillaume.sylvestre2" title="Profil Viadeo | Guillaume Sylvestre" class="fa fa-viadeo"></a></li>
+							<li><a itemprop="url" href="https://twitter.com/guillsylvestre" title="Profil Twitter" class="fa fa-twitter"></a></li>
+							<li><a itemprop="url" href="https://www.facebook.com/profile.php?id=100000234910611" title="Profil Facebook" class="fa fa-facebook"></a></li>
+						</ul>
+					</li>
+				</ul>
+				<p class="small">SIRET : 533 933 347 00015</p>
+				<p class="small">Formateur indépendant</p>
+			</section>
+		</main>
+	</div>
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-2427391-58', 'guillaumesylvestre.com');
+	  ga('send', 'pageview');
+	</script>
+</body>
+</html>
